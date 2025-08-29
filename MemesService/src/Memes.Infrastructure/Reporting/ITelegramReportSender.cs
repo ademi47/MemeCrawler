205b083094344b75ec6de2419bf48fa9f8dad3f8
Thiekshana Soysa <actmasters@gmail.com>
2025-08-29 09:28:25 +0800

    some cleanup

--- MemesService/src/Memes.Infrastructure/Reporting/ITelegramReportSender.cs
+++ MemesService/src/Memes.Infrastructure/Reporting/ITelegramReportSender.cs
@@ -1,7 +1,5 @@
-// ITelegramReportSender.cs
 public interface ITelegramReportSender
 {
     Task SendPdfReport(byte[] pdf, string filename, string caption, CancellationToken ct = default);
 }
 
-// TelegramReportSender implements ITelegramReportSender (your existing real sender)
